38a819eb36c7084ab4537096b36e970c9756afe5
Luciano Frizzera <lucaju@me.com>
2020-05-21 12:05:44 -0400

    fix(context-menu): replace "remove all" with "remove both"
    #262

--- src/js/tagContextMenu.js
+++ src/js/tagContextMenu.js
@@ -297,7 +297,7 @@ function getItems() {
         }.bind(this)
     }
     items.remove_all = {
-        name: 'Remove All',
+        name: 'Remove Both',
         icon: 'tag_remove',
         callback: function() {
             this.w.tagger.removeStructureTag(this.tagId, true);
