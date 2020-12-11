05eebfe8a948a739ae21c2a56dd37bc8a7ec1974
Luciano Frizzera <lucaju@gmail.com>
2020-12-10 21:57:38 -0500

    style: 💄 Use es6 syntax. prettier

--- src/js/tagContextMenu.js
+++ src/js/tagContextMenu.js
@@ -5,110 +5,113 @@ import { sortBy } from 'lodash';
 // import schemaNavigator from './schema/schemaNavigator3';
 import schemaNavigator from './schema/schemaNavigator4';
 
-function TagContextMenu(writer) {
-    this.w = writer;
-    this.container = `#${writer.containerId}`;
-    this.selector = `#${writer.layoutManager.$containerid}`; //`#${writer.containerId}`;
+class TagContextMenu {
+	constructor(writer) {
+		this.w = writer;
+		this.container = `#${writer.containerId}`;
+		this.selector = `#${writer.layoutManager.$containerid}`; //`#${writer.containerId}`;
 
-    // these properties are set in the show method
-    this.tagId = null;
-    this.isEntity = false;
-    this.isMultiple = false;
-    this.useSelection = false;
+		// these properties are set in the show method
+		this.tagId = null;
+		this.isEntity = false;
+		this.isMultiple = false;
+		this.useSelection = false;
 
-    // dynamically built context menu
-    $.contextMenu({
-        selector: this.selector,
-        trigger: 'none',
-        build: ($trigger, event) => {
-            return {
-                appendTo: `#${this.w.layoutManager.$containerid}`,
-                className: 'tagContextMenu cwrc',
-                animation: {
-                    duration: 0,
-                    show: 'show',
-                    hide: 'hide'
-                },
-                items: getItems.call(this),
-                callback: (key, options, event) => {
-                    // general callback used for addTagDialog and changeTagDialog
-                    const $li = $(event.target).closest('li.context-menu-item');
-                    const action = $li.data('action');
+		// dynamically built context menu
+		$.contextMenu({
+			selector: this.selector,
+			trigger: 'none',
+			// eslint-disable-next-line no-unused-vars
+			build: ($trigger, event) => {
+				return {
+					appendTo: `#${this.w.layoutManager.$containerid}`,
+					className: 'tagContextMenu cwrc',
+					animation: { duration: 0, show: 'show', hide: 'hide' },
+					items: getItems.call(this),
+					callback: (key, options, event) => {
+						// general callback used for addTagDialog and changeTagDialog
+						const $li = $(event.target).closest('li.context-menu-item');
+						const action = $li.data('action');
 
-                    if (action === undefined) return;
+						if (action === undefined) return;
 
-                    this.w.editor.currentBookmark = this.w.editor.selection.getBookmark(1);
+						this.w.editor.currentBookmark = this.w.editor.selection.getBookmark(1);
 
-                    switch (action) {
-                        case 'change':
-                            this.w.tagger.changeTagDialog(key, this.tagId);
-                            break;
-                        default:
-                            this.w.editor.currentBookmark.tagId = this.tagId;
-                            this.w.tagger.addTagDialog(key, action, this.tagId);
-                            break;
-                    }
-                }
-            }
-
-        }
-    });
-}
-
-TagContextMenu.prototype = {
-    constructor: TagContextMenu,
-
-    /**
-     * Show the tag contextmenu
-     * @param {Event} event The original contextmenu event
-     * @param {String|Array} tagId The id of the tag. Can be undefined and will be determined by tagger.getCurrentTag. Can be an array to allow for merge action.
+						switch (action) {
+							case 'change':
+								this.w.tagger.changeTagDialog(key, this.tagId);
+								break;
+							default:
+								this.w.editor.currentBookmark.tagId = this.tagId;
+								this.w.tagger.addTagDialog(key, action, this.tagId);
+								break;
+						}
+					},
+				};
+			},
+		});
+	}
+	/**
+	 * Show the tag contextmenu
+	 * @param {Event} event The original contextmenu event
+	 * @param {String|Array} tagId The id of the tag. Can be undefined and will be determined by tagger.getCurrentTag. Can be an array to allow for merge action.
+	 * @param {Boolean} useSelection
      * @param {Boolean} useSelection 
-     */
-    show: function (event, tagId, useSelection) {
-        event.preventDefault();
-        event.stopImmediatePropagation();
+	 * @param {Boolean} useSelection
+	 */
+	show(event, tagId, useSelection) {
+		event.preventDefault();
+		event.stopImmediatePropagation();
 
-        if (this.w.isReadOnly || this.w.isEditorReadOnly()) return;
+		if (this.w.isReadOnly || this.w.isEditorReadOnly()) return;
 
-        if (tagId !== undefined && Array.isArray(tagId)) {
-            this.isMultiple = true;
-            this.isEntity = false;
-            this.useSelection = false;
-        } else {
-            this.isMultiple = false;
+		if (tagId !== undefined && Array.isArray(tagId)) {
+			this.isMultiple = true;
+			this.isEntity = false;
+			this.useSelection = false;
+		} else {
+			this.isMultiple = false;
 
-            let tag = this.w.tagger.getCurrentTag(tagId)[0];
-            if (tagId === undefined) tagId = tag.getAttribute('id');
+			let tag = this.w.tagger.getCurrentTag(tagId)[0];
+			if (tagId === undefined) tagId = tag.getAttribute('id');
 
-            let tagName = tag.getAttribute('_tag');
-            if (tagName == this.w.schemaManager.getRoot() || tagName == this.w.schemaManager.getHeader()) return;
-            this.isEntity = tag.getAttribute('_entity') !== null;
-            this.useSelection = useSelection === undefined ? false : useSelection;
-        }
+			this.w.editor.selection.select(this.w.editor.dom.select('p')[0]);
 
-        this.tagId = tagId;
+			let tagName = tag.getAttribute('_tag');
+			if (
+				tagName === this.w.schemaManager.getRoot() ||
+				tagName === this.w.schemaManager.getHeader()
+			) {
+				return;
+			}
 
-        $(this.selector).contextMenu({
-            x: event.pageX,
-            y: event.pageY
-        });
-    },
+			this.isEntity = tag.getAttribute('_entity') !== null;
+			this.useSelection = useSelection === undefined ? false : useSelection;
+		}
 
-    /**
-     * Destroy the tag contextmenu
-     */
-    destroy: function () {
-        $(this.container).contextMenu('destroy');
-    }
+		this.tagId = tagId;
+
+		$(this.selector).contextMenu({
+			x: event.pageX,
+			y: event.pageY,
+		});
+	}
+
+	/**
+	 * Destroy the tag contextmenu
+	 */
+	destroy() {
+		$(this.container).contextMenu('destroy');
+	}
 }
 
 // custom menu item types
 
 // used for tracking the parent menu's action type
-$.contextMenu.types.cwrcTag = function (item, parentMenu, root) {
-    if (item.icon) this.addClass(`${root.classNames.icon} ${root.classNames.icon}-${item.icon}`);
-    $(`<span>${item.name}</span>`).appendTo(this);
-    this.data('action', parentMenu.tagAction);
+$.contextMenu.types.cwrcTag = function(item, parentMenu, root) {
+	if (item.icon) this.addClass(`${root.classNames.icon} ${root.classNames.icon}-${item.icon}`);
+	$(`<span>${item.name}</span>`).appendTo(this);
+	this.data('action', parentMenu.tagAction);
 };
 
 //Search box
@@ -123,207 +126,206 @@ $.contextMenu.types.search = function(item, opt) {
 	this.on('keyup', (e) => item.events.keyup(e, opt));
 };
 function getItems() {
-    const items = {};
+	const items = {};
 
-    if (this.tagId == null) {
-        console.warn('tagContextMenu: no tagId!');
-        return {};
-    }
+	if (this.tagId == null) {
+		console.warn('tagContextMenu: no tagId!');
+		return {};
+	}
 
-    if (this.w.isAnnotator) {
-        addEntities.call(this, items);
-        return items;
-    }
+	if (this.w.isAnnotator) {
+		addEntities.call(this, items);
+		return items;
+	}
 
-    const tag = $(`#${this.tagId}`, this.w.editor.getBody())[0];
-   
-    const needsChildTags = this.isMultiple === false;
-    const needsSiblingTags = this.isMultiple === false;
-    const needsParentTags = this.isMultiple === true || this.useSelection === false;
+	const tag = $(`#${this.tagId}`, this.w.editor.getBody())[0];
 
-    let childTags = {};
-    let siblingTags = {};
-    let parentTags = {};
+	const needsChildTags = this.isMultiple === false;
+	const needsSiblingTags = this.isMultiple === false;
+	const needsParentTags = this.isMultiple === true || this.useSelection === false;
 
-    if (needsChildTags) childTags = getChildrenForTag.call(this, tag);
-    if (needsSiblingTags) siblingTags = getSiblingsForTag.call(this, tag);
-    if (needsParentTags) parentTags = getInsertAroundTags.call(this, tag);
+	let childTags = {};
+	let siblingTags = {};
+	let parentTags = {};
 
-    if (this.isMultiple) {
-        items.add_tag_around = {
-            name: 'Insert Tag Around',
-            icon: 'tag_add',
-            tagAction: 'around',
-            items: function () {
-                return parentTags;
-            }()
-        }
+	if (needsChildTags) childTags = getChildrenForTag.call(this, tag);
+	if (needsSiblingTags) siblingTags = getSiblingsForTag.call(this, tag);
+	if (needsParentTags) parentTags = getInsertAroundTags.call(this, tag);
 
-        items.sep0 = '---';
+	if (this.isMultiple) {
+		items.add_tag_around = {
+			name: 'Insert Tag Around',
+			icon: 'tag_add',
+			tagAction: 'around',
+			items: (function() {
+				return parentTags;
+			})(),
+		};
 
-        items.merge_tags = {
-            name: 'Merge Tags',
-            icon: 'tag_merge',
-            callback: () => {
-                const tags = $(`'#${this.tagId.join(',#')}`, this.w.editor.getBody());
-                this.w.tagger.mergeTags(tags);
-            }
-        }
+		items.sep0 = '---';
 
-        return items;
-    }
+		items.merge_tags = {
+			name: 'Merge Tags',
+			icon: 'tag_merge',
+			callback: () => {
+				const tags = $(`'#${this.tagId.join(',#')}`, this.w.editor.getBody());
+				this.w.tagger.mergeTags(tags);
+			},
+		};
 
-    if (this.useSelection) {
-        items.add_tag = {
-            name: 'Insert Tag',
-            icon: 'tag_add',
-            tagAction: 'add',
-            items: function () {
-                return childTags;
-            }()
-        }
-        if (this.w.schemaManager.isSchemaCustom() === false) {
-            addEntities.call(this, items);
-        }
+		return items;
+	}
 
-        items.sep0 = '---';
-    }
+	if (this.useSelection) {
+		items.add_tag = {
+			name: 'Insert Tag',
+			icon: 'tag_add',
+			tagAction: 'add',
+			items: (function() {
+				return childTags;
+			})(),
+		};
+		if (this.w.schemaManager.isSchemaCustom() === false) {
+			addEntities.call(this, items);
+		}
 
-    if (!this.useSelection) {
+		items.sep0 = '---';
+	}
 
-        items.add_tag_before = {
-            name: 'Insert Tag Before',
-            icon: 'tag_add',
-            tagAction: 'before',
-            items: function () {
-                return siblingTags;
-            }()
-        }
+	if (!this.useSelection) {
+		items.add_tag_before = {
+			name: 'Insert Tag Before',
+			icon: 'tag_add',
+			tagAction: 'before',
+			items: (function() {
+				return siblingTags;
+			})(),
+		};
 
-        items.add_tag_after = {
-            name: 'Insert Tag After',
-            icon: 'tag_add',
-            tagAction: 'after',
-            items: function () {
-                return siblingTags;
-            }()
-        }
+		items.add_tag_after = {
+			name: 'Insert Tag After',
+			icon: 'tag_add',
+			tagAction: 'after',
+			items: (function() {
+				return siblingTags;
+			})(),
+		};
 
-        items.add_tag_around = {
-            name: 'Insert Tag Around',
-            icon: 'tag_add',
-            tagAction: 'around',
-            items: function () {
-                return parentTags;
-            }()
-        }
+		items.add_tag_around = {
+			name: 'Insert Tag Around',
+			icon: 'tag_add',
+			tagAction: 'around',
+			items: (function() {
+				return parentTags;
+			})(),
+		};
 
-        items.add_tag_inside = {
-            name: 'Insert Tag Inside',
-            icon: 'tag_add',
-            tagAction: 'inside',
-            items: function () {
-                return childTags;
-            }()
-        }
+		items.add_tag_inside = {
+			name: 'Insert Tag Inside',
+			icon: 'tag_add',
+			tagAction: 'inside',
+			items: (function() {
+				return childTags;
+			})(),
+		};
 
-        items.sep1 = '---';
-    }
+		items.sep1 = '---';
+	}
 
-    items.edit_tag = {
-        name: 'Edit Tag/Entity',
-        icon: 'tag_edit',
-        callback: () => this.w.tagger.editTagDialog(this.tagId)
-    }
+	items.edit_tag = {
+		name: 'Edit Tag/Entity Annotation',
+		icon: 'tag_edit',
+		callback: () => this.w.tagger.editTagDialog(this.tagId),
+	};
 
-    if (!this.isEntity) {
-        const tagName = tag.getAttribute('_tag');
-        if (this.w.schemaManager.isTagEntity(tagName)) {
-            items.convert_tag = {
-                name: 'Convert to Entity',
-                icon: 'tag_edit',
-                callback: () => this.w.schemaManager.mapper.convertTagToEntity(tag, true)
-            }
-        }
-    }
+	if (!this.isEntity) {
+		const tagName = tag.getAttribute('_tag');
+		if (this.w.schemaManager.isTagEntity(tagName)) {
+			items.convert_tag = {
+				name: 'Convert to Entity Annotation',
+				icon: 'tag_edit',
+				callback: () => this.w.schemaManager.mapper.convertTagToEntity(tag, true),
+			};
+		}
+	}
 
-    // ! Change tag should considers the filters applied to tag availability.
-    // e.g. if the tag in question is part of a OPTIONAL CHOICE patterns, the filter would have removed other options in this patterns
-    // That is, filter should be applied depending onf the case of use and context.
-    items.change_tag = {
-        name: 'Change Tag',
-        icon: 'tag_edit',
-        tagAction: 'change',
-        items: function () {
-            return siblingTags;
-        }()
-    }
+	// ! Change tag should considers the filters applied to tag availability.
+	// e.g. if the tag in question is part of a OPTIONAL CHOICE patterns, the filter would have removed other options in this patterns
+	// That is, filter should be applied depending onf the case of use and context.
+	items.change_tag = {
+		name: 'Change Tag',
+		icon: 'tag_edit',
+		tagAction: 'change',
+		items: (function() {
+			return siblingTags;
+		})(),
+	};
 
-    if (this.isEntity) {
-        items.copy_entity = {
-            name: 'Copy Entity',
-            icon: 'tag_copy',
-            callback: () => this.w.tagger.copyTag(this.tagId)
-        }
-    } else {
-        items.copy_tag = {
-            name: 'Copy Tag and Contents',
-            icon: 'tag_copy',
-            callback: () => this.w.tagger.copyTag(this.tagId)
-        }
-    }
+	if (this.isEntity) {
+		items.copy_entity = {
+			name: 'Copy Entity',
+			icon: 'tag_copy',
+			callback: () => this.w.tagger.copyTag(this.tagId),
+		};
+	} else {
+		items.copy_tag = {
+			name: 'Copy Tag and Contents',
+			icon: 'tag_copy',
+			callback: () => this.w.tagger.copyTag(this.tagId),
+		};
+	}
 
-    if (this.w.editor.copiedElement.element !== null) {
-        items.paste_tag = {
-            name: 'Paste Tag',
-            icon: 'tag_paste',
-            callback: () => this.w.tagger.pasteTag()
-        }
-    } else if (this.w.editor.copiedEntity !== null) {
-        items.paste_entity = {
-            name: 'Paste Entity',
-            icon: 'tag_paste',
-            callback: () => this.w.tagger.pasteEntity()
-        }
-    }
+	if (this.w.editor.copiedElement.element !== null) {
+		items.paste_tag = {
+			name: 'Paste Tag',
+			icon: 'tag_paste',
+			callback: () => this.w.tagger.pasteTag(),
+		};
+	} else if (this.w.editor.copiedEntity !== null) {
+		items.paste_entity = {
+			name: 'Paste Entity',
+			icon: 'tag_paste',
+			callback: () => this.w.tagger.pasteEntity(),
+		};
+	}
 
-    if (this.useSelection) {
-        items.split_tag = {
-            name: 'Split Tag',
-            icon: 'tag_split',
-            callback: () => this.w.tagger.splitTag()
-        }
-    }
+	if (this.useSelection) {
+		items.split_tag = {
+			name: 'Split Tag',
+			icon: 'tag_split',
+			callback: () => this.w.tagger.splitTag(),
+		};
+	}
 
-    items.sep2 = '---';
+	items.sep2 = '---';
 
-    if (this.isEntity) {
-        items.remove_entity = {
-            name: 'Remove Entity',
-            icon: 'tag_remove',
-            callback: () => this.w.tagger.removeEntity(this.tagId)
-        }
-    }
+	if (this.isEntity) {
+		items.remove_entity = {
+			name: 'Remove Entity',
+			icon: 'tag_remove',
+			callback: () => this.w.tagger.removeEntity(this.tagId),
+		};
+	}
 
-    items.remove_tag = {
-        name: 'Remove Tag',
-        icon: 'tag_remove',
-        callback: () => this.w.tagger.removeStructureTag(this.tagId, false)
-    }
+	items.remove_tag = {
+		name: 'Remove Tag',
+		icon: 'tag_remove',
+		callback: () => this.w.tagger.removeStructureTag(this.tagId, false),
+	};
 
-    items.remove_content = {
-        name: 'Remove Content Only',
-        icon: 'tag_remove',
-        callback: () => this.w.tagger.removeStructureTagContents(this.tagId)
-    }
+	items.remove_content = {
+		name: 'Remove Content Only',
+		icon: 'tag_remove',
+		callback: () => this.w.tagger.removeStructureTagContents(this.tagId),
+	};
 
-    items.remove_all = {
-        name: 'Remove All',
-        icon: 'tag_remove',
-        callback: () => this.w.tagger.removeStructureTag(this.tagId, true)
-    }
+	items.remove_all = {
+		name: 'Remove All',
+		icon: 'tag_remove',
+		callback: () => this.w.tagger.removeStructureTag(this.tagId, true),
+	};
 
-    return items;
+	return items;
 }
 
 function addEntities(items) {
