1f790db777e2d6d16dd918a1141a85b0984da779
Luciano Frizzera <lucaju@gmail.com>
2021-05-05 10:02:23 -0400

    style: 💄 Start using prettier to format code

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-	collectCoverage: true,
-	collectCoverageFrom: [
-		'./src/js/**/*',
-		'!**/js/tinymce_plugins/**',
-		'!**/schema/cwrcEntry/**',
-		'!**/schema/orlando/**',
-		'!**/schema/teiLite/**',
-	],
-	coverageDirectory: './coverage',
-	coverageThreshold: {
-		global: {
-			branches: 35,
-			functions: 50,
-			lines: 50,
-			statements: 50,
-		},
-	},
-	setupFiles: ['./test/test-env.js'],
-	runner: '@jest-runner/electron',
-	testEnvironment: '@jest-runner/electron/environment',
-	testMatch: ['**/test/test.js'],
-	transform: {
-		'.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-		'^.+\\.js$': 'babel-jest',
-	},
-	verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/js/**/*',
+    '!**/js/tinymce_plugins/**',
+    '!**/schema/cwrcEntry/**',
+    '!**/schema/orlando/**',
+    '!**/schema/teiLite/**',
+  ],
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 35,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  setupFiles: ['./test/test-env.js'],
+  runner: '@jest-runner/electron',
+  testEnvironment: '@jest-runner/electron/environment',
+  testMatch: ['**/test/test.js'],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.js$': 'babel-jest',
+  },
+  verbose: true,
 };
