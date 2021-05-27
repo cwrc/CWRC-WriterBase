b0665215264610524cc5540726eb191a87581863
Luciano Frizzera <lucaju@gmail.com>
2021-05-27 09:35:25 -0400

    fix: 🐛 move jquery 3,5 workaround to its own folder

--- src/js/writer.js
+++ src/js/writer.js
@@ -1,10 +1,5 @@
 import $ from 'jquery';
-
-// JQuery Hack: revert behaviour introduced in v.3.5
-// See: https://jquery.com/upgrade-guide/3.5/
-// eslint-disable-next-line no-useless-escape
-const rxhtmlTag = /<(?!area|br|col|embed|hr|img|input|link|meta|param)(([a-z][^\/\0>\x20\t\r\n\f]*)[^>]*)\/>/gi;
-$.htmlPrefilter = (html) => html.replace( rxhtmlTag, '<$1></$2>' );
+import '../lib/jquery/jquery_3.5_workaround.js';
 
 const EventManager = require('./eventManager.js');
 import Utilities from './utilities';
