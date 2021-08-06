d6379fb193c86d354b34f554cb14ec60722f80ca
Luciano Frizzera <lucaju@gmail.com>
2021-08-06 13:06:31 -0400

    refactor: 💡 tweak settings component

--- src/js/dialogs/settingComponents/HeaderMenuOptions.js
+++ src/js/dialogs/settingComponents/HeaderMenuOptions.js
@@ -1,34 +1,34 @@
 import { Box, Icon, IconButton } from '@material-ui/core';
-import React from 'react';
+import React, { Component } from 'react';
 
-const HeaderMenuOptions = ({ dialog, helpUrl }) => {
-	const helpLink = helpUrl ?? 'https://cwrc.ca/Documentation/CWRC-Writer';
-	const openDialog = () => dialog?.dialog('open') ?? undefined;
+export default class HeaderMenuOptions extends Component {
+	helpLink = this.props.helpUrl ?? 'https://cwrc.ca/Documentation/CWRC-Writer';
+	openDialog = () => this.props.dialog.dialog('open');
 
-	return (
-		<Box>
-			<IconButton
-				aria-label="settings"
-				className="settingsLink"
-				onClick={openDialog}
-				size="small"
-				style={{ color: '#ffffff' }}
-			>
-				<Icon fontSize="small">settings</Icon>
-			</IconButton>
-			<IconButton
-				aria-label="help"
-				className="helpLink"
-				href={helpLink}
-				rel="noopener noreferrer"
-				size="small"
-				style={{ marginLeft: '8px', color: '#ffffff' }}
-				target="_blank"
-			>
-				<Icon fontSize="small">help</Icon>
-			</IconButton>
-		</Box>
-	);
-};
-
-export default HeaderMenuOptions;
+	render() {
+		return (
+			<Box>
+				<IconButton
+					aria-label="settings"
+					className="settingsLink"
+					onClick={this.openDialog}
+					size="small"
+					style={{ color: '#ffffff' }}
+				>
+					<Icon fontSize="small">settings</Icon>
+				</IconButton>
+				<IconButton
+					aria-label="help"
+					className="helpLink"
+					href={this.helpLink}
+					rel="noopener noreferrer"
+					size="small"
+					style={{ marginLeft: '8px', color: '#ffffff' }}
+					target="_blank"
+				>
+					<Icon fontSize="small">help</Icon>
+				</IconButton>
+			</Box>
+		);
+	}
+}
